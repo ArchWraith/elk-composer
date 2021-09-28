77a8e501e78ed69e75c4fcced6fa111be405baaf
Ferullo <ferullo@wsip-70-164-71-213.ok.ok.cox.net>
2021-09-28 16:07:42 -0500

    h

--- compose.sh
+++ compose.sh
@@ -1,3 +1,5 @@
 docker-compose -f docker-compose.yml up \
     --abort-on-container-exit \
     --exit-code-from elasticsearch
+
+docker --config ~/.dso pull registry1.dso.mil/ironbank/elastic/elasticsearch/elasticsearch:7.14.1
