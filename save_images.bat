c3e8557596e82780f0063f85e13784c63e6ef358
Ferullo <ferullo@wsip-70-164-71-221.ok.ok.cox.net>
2021-12-03 13:10:32 -0600

    h

--- save_images.bat
+++ save_images.bat
@@ -1,6 +1,6 @@
-docker save registry1.dso.mil/ironbank/elastic/elasticsearch/elasticsearch:7.14.1 > images/elasticsearch.tar.gz
+docker save -o images/elasticsearch.tar.gz registry1.dso.mil/ironbank/elastic/elasticsearch/elasticsearch:7.14.1 
 echo "elasticsearch saved"
-docker save registry1.dso.mil/ironbank/elastic/kibana/kibana:7.14.0 > images/kibana.tar.gz
+docker save -o images/kibana.tar.gz registry1.dso.mil/ironbank/elastic/kibana/kibana:7.14.0
 echo "kibana saved"
-docker save registry1.dso.mil/ironbank/elastic/logstash/logstash:7.14.0 > images/logstash.tar.gz
+docker save -o  images/logstash.tar.gz registry1.dso.mil/ironbank/elastic/logstash/logstash:7.14.0 
 echo "logstash saved"
